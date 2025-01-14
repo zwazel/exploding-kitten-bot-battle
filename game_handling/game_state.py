dc5758a21f370f86d84aba9f308e010c77cd970d
Tim Irmler <zwazel@hotmail.com>
2025-01-14 22:47:56 +0100

    improved comment, maybe?

--- game_handling/game_state.py
+++ game_handling/game_state.py
@@ -9,7 +9,11 @@ class GameState:
     total_cards_in_deck: CardCounts
     # the amount of cards left in the draw deck
     cards_left: int
-    # This is TRUE if the last card drawn was an exploding kitten and was RETURNED to the deck by the last player (he had a defuse card and didnt explode)
+    '''
+    This is TRUE if the last card drawn was an exploding kitten and was RETURNED to the deck by the last player (he had a defuse card and didn't explode).
+    This is FALSE if the last card drawn was an exploding kitten and was NOT RETURNED to the deck by the last player (he didn't have a defuse card and exploded).
+    This is also FALSE if the last card drawn was NOT an exploding kitten.
+    '''
     was_last_card_exploding_kitten: bool
     # the history of the cards played
     history_of_played_cards: list[Card]
